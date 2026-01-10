4fad51cc5a1f7ca619c3f6f396b8ccefb26ceaa5
Asif Ali <Asifali2735@gmail.com>
2026-01-10 16:33:15 +0000

    but i deploy my app on vercel but backend is not working why

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
+
 export const firebaseConfig = {
   apiKey: "AIzaSyCoEHuUbjD5lQh1YsdhtF0CdZ4BzcF9ZF4",
   authDomain: "jannu-live001.firebaseapp.com",
   projectId: "jannu-live001",
-  storageBucket: "jannu-live001.firebasestorage.app",
+  storageBucket: "jannu-live001.appspot.com",
   messagingSenderId: "432039192606",
   appId: "1:432039192606:web:2f47f160055fde99fd660f",
   measurementId: "G-W4J0JD0PY7"
